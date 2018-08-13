434729e7aae045677cf919c96cb452fe4d900bc0
t0m3k52 <tomek.szpytma@gmail.com>
2018-08-13 20:39:05 +0200

    added catch ut

---
+++ test_catch/Balanced.test.cpp
@@ -0,0 +1,16 @@
+#include "catch.hpp"
+#include "../inc/Balanced.hpp"
+
+SCENARIO("test")
+{
+    GIVEN("test")
+    {
+        WHEN("ex:")
+        {
+            THEN("testing")
+            {
+                REQUIRE(true);
+            }
+        }
+    }
+}
